62caa3097ba2cebd9e12480da609c5c2b5734cac
facenomore <afacenomore@gmail.com>
2017-02-14 12:39:18 +0200

    Initial commit

---
+++ src/main/java/net/facenomore/dao/UserDao.java
@@ -0,0 +1,17 @@
+package net.facenomore.dao;
+
+import net.facenomore.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    public void addUser(User user);
+
+    public void updateUser(User user);
+
+    public void removeUser(int id);
+
+    public User getUserById(int id);
+
+    public List<User> listUsers();
+}
